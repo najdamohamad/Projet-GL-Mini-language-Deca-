4a7c7d26c2b718401c7593dc95471686871fdee1
Equipe gl47 <gl47@gitlab.ensimag.fr>
2021-12-14 16:21:03 +0100

    Personalisation du dépôt pour gl47

--- src/main/java/fr/ensimag/deca/tree/Modulo.java
+++ src/main/java/fr/ensimag/deca/tree/Modulo.java
@@ -8,8 +8,8 @@ import fr.ensimag.deca.context.EnvironmentExp;
 
 /**
  *
- * @author @AUTHOR@
- * @date @DATE@
+ * @author gl47
+ * @date 01/01/2022
  */
 public class Modulo extends AbstractOpArith {
 
