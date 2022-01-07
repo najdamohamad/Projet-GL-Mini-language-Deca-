5ed0f50da10ea096f695c43f1bfdd812589e3a82
xy2_ <hugo@xy2.dev>
2022-01-07 12:26:57 +0100

    Add empty main test

--- src/test/java/fr/ensimag/deca/context/helloworld/TestEmptyMain.java
+++ src/test/java/fr/ensimag/deca/context/helloworld/TestEmptyMain.java
@@ -1,7 +1,22 @@
 package fr.ensimag.deca.context.helloworld;
 
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.tree.*;
+import org.junit.jupiter.api.Test;
+
 /**
  * Test for the empty main node.
+ *
+ * @date 07/01/2022
  */
 public class TestEmptyMain {
+    @Test
+    public void testEmptyMain() throws ContextualError {
+        DecacCompiler compiler = new DecacCompiler(null, null);
+        AbstractMain emptyMain = new EmptyMain();
+        AbstractProgram program = new Program(new ListDeclClass(), emptyMain);
+
+        program.verifyProgram(compiler);
+    }
 }
