c7650beb8c9a9ddc72c31d59d97832fba558d556
xy2_ <hugo@xy2.dev>
2022-01-17 19:09:07 +0100

    ci

--- source-pipeline.sh
+++ source-pipeline.sh
@@ -13,6 +13,12 @@ echo $(get_abs_filename './ima')
 echo $(get_abs_filename '')
 PATH="$PATH:$(get_abs_filename './ima')"
 chmod +x ./ima/ima-x86_64-Linux
+<<<<<<< HEAD
 # QEMU related, otherwhise doesn't run in vm, wtf?
+=======
+
+# QEMU related, otherwhise doesn't run, wtf?
+# "bug": https://gitlab.com/qemu-project/qemu/-/issues/447
+>>>>>>> fa2ec88 (ci)
 # https://docs.windriver.com/fr-FR/bundle/Wind_River_Linux_Release_Notes_LTS_21_tki1589820771450/page/yiz1629749231871.html
 echo "vm.mmap_min_addr = 65536" > /etc/sysctl.d/mmap_min_addr.conf
