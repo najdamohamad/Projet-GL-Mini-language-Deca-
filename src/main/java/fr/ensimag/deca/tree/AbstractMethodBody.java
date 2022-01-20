68f6d180a51554a5875be7200e8eb14a5c9fd14d
Mahmoud MAZOUZ <mahmoud.mazouz@ensimag.grenoble-inp.fr>
2022-01-20 02:18:23 +0100

    implement rule (3.15) of pass 3

--- src/main/java/fr/ensimag/deca/tree/AbstractMethodBody.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractMethodBody.java
@@ -4,6 +4,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
 
 /**
  * Binary expressions.
@@ -24,7 +25,7 @@ public abstract class AbstractMethodBody extends Tree {
      *                     the synthetized attribute
      * @param currentClass corresponds to the "class" attribute (null in the main bloc).
      */
-    protected abstract void verifyMethodBody(DecacCompiler compiler,
-                                             EnvironmentExp localEnv, ClassDefinition currentClass)
+    protected abstract void verifyMethodBody(DecacCompiler compiler, EnvironmentExp localEnv,
+                                             ClassDefinition currentClass, Type returnType)
             throws ContextualError;
 }
--- src/main/java/fr/ensimag/deca/tree/MethodBody.java
+++ src/main/java/fr/ensimag/deca/tree/MethodBody.java
@@ -4,6 +4,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import org.apache.commons.lang.Validate;
 
@@ -47,7 +48,7 @@ public class MethodBody extends AbstractMethodBody {
 
     @Override
     protected void verifyMethodBody(DecacCompiler compiler, EnvironmentExp localEnv,
-                                    ClassDefinition currentClass) throws ContextualError {
+                                    ClassDefinition currentClass, Type returnType) throws ContextualError {
         throw new UnsupportedOperationException("not yet implemented");
     }
 }
