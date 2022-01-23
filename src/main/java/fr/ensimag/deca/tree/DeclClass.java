80ec93a43557e7ae2aeeb0f3d7da9f540a4107ab
xy2_ <hugo@xy2.dev>
2022-01-23 11:16:59 +0100

    DeclClass: make sure to use programInit instead of program
    ...when constructing method table

--- src/main/java/fr/ensimag/deca/tree/DeclClass.java
+++ src/main/java/fr/ensimag/deca/tree/DeclClass.java
@@ -140,22 +140,22 @@ public class DeclClass extends AbstractDeclClass {
         int stackUsage = 0;
         IMAProgram programInit = new IMAProgram(program);
         LOG.debug("codegen "+className);
-        DAddr position = new RegisterOffset(program.getStackUsage() + 1, Register.GB);
+        DAddr position = new RegisterOffset(programInit.getStackUsage() + 1, Register.GB);
         className.getClassDefinition().setMethodTableAddr(position);
         int placeDansLeStack = 1;
         DAddr positionMere = superClassName.getClassDefinition().getMethodTableAddr();
         programInit.addInstruction(new LEA(positionMere, Register.R0));
         programInit.addInstruction(new STORE(Register.R0, position));
-        program.bumpStackUsage();
+        programInit.bumpStackUsage();
         for (AbstractDeclMethod method : listDeclMethod.getList()) {
             className.getClassDefinition().listMethod.add(method.getMethodName().getName().toString());
         }
         // Init table method inherited
         for (String SuperMethodName : superClassName.getClassDefinition().listMethod){
             if (!className.getClassDefinition().listMethod.contains(SuperMethodName)){
-                program.addInstruction(new LOAD(new LabelOperand(new Label(SuperMethodName)), Register.R0));
-                program.addInstruction(new STORE(Register.R0, new RegisterOffset(placeDansLeStack, Register.GB)));
-                program.bumpStackUsage();
+                programInit.addInstruction(new LOAD(new LabelOperand(new Label(SuperMethodName)), Register.R0));
+                programInit.addInstruction(new STORE(Register.R0, new RegisterOffset(placeDansLeStack, Register.GB)));
+                programInit.bumpStackUsage();
                 className.getClassDefinition().listMethod.add(SuperMethodName);
                 placeDansLeStack += 1;
             }
@@ -163,7 +163,7 @@ public class DeclClass extends AbstractDeclClass {
         // Init table method
         for (AbstractDeclMethod method : listDeclMethod.getList()) {
             placeDansLeStack = method.codeGenInitTable(programInit, placeDansLeStack);
-            program.bumpStackUsage();
+            programInit.bumpStackUsage();
         }
         // Init our fields to 0.
         for (AbstractDeclField declField : listDeclField.getList()) {
